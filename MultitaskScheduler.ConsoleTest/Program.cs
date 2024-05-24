bd1f2b8de66c5b5aa6418da82d700ddae437b608
Farid Mustafayev <fmustafayev@spherasolutions.com>
2024-05-23 21:52:19 -0400

    Extended console test

--- MultitaskScheduler.ConsoleTest/Program.cs
+++ MultitaskScheduler.ConsoleTest/Program.cs
@@ -5,14 +5,17 @@ namespace MultitaskScheduler.ConsoleTest
 {
     internal class Program
     {
-        private static Scheduler _scheduler = Scheduler.Factory.CreateNew("Default Scheduler");
+        private static readonly Scheduler _scheduler = Scheduler.Factory.CreateNew("Default Scheduler");
 
         static void Main(string[] args)
         {
-            _scheduler.ScheduleJob("Job 1", 4, () => Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")));
+            Console.WriteLine("Schedule Job 1 to run every 4 seconds: " + DateTime.Now.ToString("HH:mm:ss"));
+            _scheduler.ScheduleJob("Job 1", 4, () => Console.WriteLine("Job 1: " + DateTime.Now.ToString("HH:mm:ss")));
             Thread.Sleep(9000);
+            Console.WriteLine("Pause Job 1: " + DateTime.Now.ToString("HH:mm:ss"));
             _scheduler.IsPaused = true;
             Thread.Sleep(9000);
+            Console.WriteLine("Continue Job 1: " + DateTime.Now.ToString("HH:mm:ss"));
             _scheduler.IsPaused = false;
             Console.ReadKey();
         }
